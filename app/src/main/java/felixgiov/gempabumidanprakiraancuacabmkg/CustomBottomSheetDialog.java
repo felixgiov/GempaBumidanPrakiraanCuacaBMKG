8d206f254279e525dad983144cb3026ba767fdde
Felix Giovanni <gv.felix@yahoo.co.id>
2017-01-11 10:56:25 +0700

    Bottom Sheet Fragment Toolbar fixed.

--- app/src/main/java/felixgiov/gempabumidanprakiraancuacabmkg/CustomBottomSheetDialog.java
+++ app/src/main/java/felixgiov/gempabumidanprakiraancuacabmkg/CustomBottomSheetDialog.java
@@ -6,9 +6,14 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -65,5 +70,4 @@ public class CustomBottomSheetDialog extends BottomSheetDialogFragment {
 
         return view;
     }
-
 }
